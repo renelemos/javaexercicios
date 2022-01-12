57b2cc3364afa9b9e765e8b4a40c8c11ccfb3fc1
Renê Lemos <87790784+renelemos@users.noreply.github.com>
2022-01-11 22:07:14 -0300

    Add files via upload

---
+++ CompraTeste.java
@@ -0,0 +1,22 @@
+package oo.composicao;
+
+public class CompraTeste {
+
+	public static void main(String[] args) {
+			
+	Compra compra1 = new Compra();
+	compra1.cliente = "Joao Pedro";
+	compra1.adicionarItem(new Item("Caneta", 20, 7.45));
+	compra1.adicionarItem(new Item("Borracha", 12, 3.89));
+	compra1.adicionarItem(new Item("Caneta", 3, 18.79));
+	
+	System.out.println(compra1.itens.size());
+	System.out.println(compra1.getValorTotal());
+	
+	//Só para mostrar a relação bidirecional!
+	double total = compra1.itens.get(0).compra
+			.itens.get(1).compra.getValorTotal();
+	System.out.println("O valor total é " + total);
+	
+}
+}
